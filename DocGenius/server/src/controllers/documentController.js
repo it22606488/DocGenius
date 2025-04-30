cfd06f2a2bd776f22718328e47b28398b8c2d97b
Hasanka Dinuwantha <122743041+HasankaDinuwantha@users.noreply.github.com>
2025-04-30 20:01:32 +0530

    change comment
    change comment

--- DocGenius/server/src/controllers/documentController.js
+++ DocGenius/server/src/controllers/documentController.js
@@ -18,7 +18,8 @@ exports.getDocuments = async (req, res) => {
   }
 };
 
-// Get a single document
+// Get a single doc
+
 exports.getDocument = async (req, res) => {
   try {
     const document = await Document.findById(req.params.id);
